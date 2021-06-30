1fa87da6da70c99e9bee1ad74de607e325938801
Jon Diep <jondiep1@gmail.com>
2021-06-30 16:53:47 -0700

    fix: update hook

--- hooks/useMerkleDistributorContract.ts
+++ hooks/useMerkleDistributorContract.ts
@@ -7,15 +7,17 @@ import MerkleDistributor from '../data/MerkleDistributor.sol/MerkleDistributor.j
 export function useMerkleDistributorContract(): [Contract | undefined, Dispatch<SetStateAction<string | undefined>>] {
   const { account, library } = useWeb3React()
 
+  const [prevAccount, setPrevAccount] = useState<string>()
   const [contract, setContract] = useState<Contract>()
   const [address, setAddress] = useState<string>()
 
   useEffect(() => {
-    if (account && library && !contract && address) {
+    if (account && library && (!contract || account !== prevAccount) && address) {
       const signer = library.getSigner(account)
+      setPrevAccount(account)
       setContract(new Contract(address, MerkleDistributor.abi, signer))
     }
-  }, [account, library, contract, address])
+  }, [account, library, contract, address, prevAccount])
 
   return [contract, setAddress]
 }
